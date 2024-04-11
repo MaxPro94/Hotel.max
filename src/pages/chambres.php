b8455687e218069254ca251a8e0b810f0c8ff9f6
Maxime Thomé <thomemaximepro@gmail.com>
2024-04-11 17:41:28 +0200

    Opti requete SQL

--- src/pages/chambres.php
+++ src/pages/chambres.php
@@ -2,7 +2,7 @@
 
 session_start();
 $title = 'Les chambres';
-$requete = $maDB->prepare("SELECT chambre.*, chambre_hotel.*, hotel.id_hotel FROM chambre JOIN chambre_hotel JOIN hotel WHERE chambre.id_chambre = chambre_hotel.id_chambre AND chambre_hotel.id_hotel = hotel.id_hotel;");
+$requete = $maDB->prepare("SELECT chambre.*, chambre_hotel.*, hotel.id_hotel FROM chambre JOIN chambre_hotel ON chambre.id_chambre = chambre_hotel.id_chambre JOIN hotel ON chambre_hotel.id_hotel = hotel.id_hotel;");
 $requete->execute();
 
 $resultats_chambres = $requete->fetchAll();
